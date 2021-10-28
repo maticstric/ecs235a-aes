cbaf683815a0d9a5aec02cadbb830123ef6700c0
maticstric <matic@likar.me>
2021-10-28 16:19:38 -0700

    Add function to find differential trails and add decryption

--- spn.py
+++ spn.py
@@ -2,6 +2,7 @@
 # Differential cryptanalysis based on the same book
 
 SBOX = [0xe, 0x4, 0xd, 0x1, 0x2, 0xf, 0xb, 0x8, 0x3, 0xa, 0x6, 0xc, 0x5, 0x9, 0x0, 0x7]
+INV_SBOX = [0xe, 0x3, 0x4, 0x8, 0x1, 0xc, 0xa, 0xf, 0x7, 0xd, 0x9, 0x6, 0xb, 0x2, 0x0, 0x5]
 
 KEY1 = [0x7, 0x9, 0x0, 0x3]
 KEY2 = [0xd, 0x1, 0x0, 0xf]
@@ -17,6 +18,51 @@ def main():
     encrypt(state)
 
     print_1d_hex(state)
+
+    decrypt(state)
+
+    print_1d_hex(state)
+
+    diff_dist_table = build_difference_distribution_table(SBOX)
+
+    find_differential_trail([0xb, 0x0, 0xb, 0x0], diff_dist_table)
+
+def find_differential_trail(input_xor, diff_dist_table):
+    probability = 1
+    active_sboxes = [False, False, False, False] # bool array of which sboxes are active
+    current_xor = input_xor.copy()
+
+    for r in range(3):
+        for i in range(len(current_xor)):
+            if current_xor[i] > 0: active_sboxes[i] = True
+            else: active_sboxes[i] = False
+
+        # XOR going through the sbox (source of nonlinearity)
+        for i in range(len(active_sboxes)):
+            if active_sboxes[i]:
+                max_probability = max(diff_dist_table[current_xor[i]])
+                most_probable_output_xor = diff_dist_table[current_xor[i]].index(max_probability)
+
+                probability *= max_probability / len(diff_dist_table)
+                
+                current_xor[i] = most_probable_output_xor
+
+        # XOR going through the permutation (linear)
+        permutate(current_xor)
+    
+    print(probability)
+
+def build_difference_distribution_table(sbox):
+    diff_dist_table = [[0 for i in range(len(sbox))] for j in range(len(sbox))]
+
+    for x_prime in range(16):
+        for x in range(16):
+            x_star = x ^ x_prime
+            y_prime = sbox[x] ^ sbox[x_star]
+
+            diff_dist_table[x_prime][y_prime] += 1
+
+    return diff_dist_table
 
 
 """ SPN Cipher """
@@ -39,6 +85,24 @@ def encrypt(state):
 
     add_round_key(state, KEY5)
 
+def decrypt(state):
+    add_round_key(state, KEY5)
+
+    inv_substitute(state)
+    add_round_key(state, KEY4)
+
+    inv_permutate(state)
+    inv_substitute(state)
+    add_round_key(state, KEY2)
+
+    inv_permutate(state)
+    inv_substitute(state)
+    add_round_key(state, KEY2)
+
+    inv_permutate(state)
+    inv_substitute(state)
+    add_round_key(state, KEY1)
+
 def substitute(state):
     # state = array of nibbles, not bytes  
     for i in range(len(state)):
@@ -52,6 +116,14 @@ def permutate(state):
     state[1] = ((tmp[0] & 0x4) << 1) + (tmp[1] & 0x4) + ((tmp[2] & 0x4) >> 1) + ((tmp[3] & 0x4) >> 2)
     state[2] = ((tmp[0] & 0x2) << 2) + ((tmp[1] & 0x2) << 1) + (tmp[2] & 0x2) + ((tmp[3] & 0x2) >> 1)
     state[3] = ((tmp[0] & 0x1) << 3) + ((tmp[1] & 0x1) << 2) + ((tmp[2] & 0x1) << 1) + (tmp[3] & 0x1)
+
+def inv_substitute(state):
+    # state = array of nibbles, not bytes  
+    for i in range(len(state)):
+        state[i] = INV_SBOX[state[i]]
+
+def inv_permutate(state): # In the specific case of this permutation box, inv is the same
+    permutate(state)
 
 def add_round_key(state, key):
     # state, key = array of nibbles, not bytes  
